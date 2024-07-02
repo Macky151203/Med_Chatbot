37b75b74adf9a9391311be8c8bfd58fa31b897be
subramaniyan <subramaniyansrinivasan2003@gmail.com>
2024-07-02 21:24:36 +0530

    connected db

--- app/[uid]/page.tsx
+++ app/[uid]/page.tsx
@@ -1,9 +1,24 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { db } from '../../firebaseconfig'
+import { doc, getDoc } from 'firebase/firestore'
 
 export default function Mainpage({params}:{params:{"uid":string}}) {
   const uid=params['uid']
   console.log("uid id-",uid)
+  useEffect(()=>{
+    const getData= async()=>{
+      const docRef=doc(db,'orgpapptinfo',uid);
+      const docSnap=await getDoc(docRef)
+      if(docSnap.exists()){
+        console.log("This is the data- ",docSnap.data().appt)
+      }
+      else{
+        console.log("Error fetching data")
+      }
+    }
+    getData();
+  },[])
   return (
     <div>Mainpage Forms</div>
   )
---
+++ firebaseconfig.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBI7QJjr4P7-rmCHrx4GeQ0b_uAnYLlL5U",
+  authDomain: "cweb2-dev.firebaseapp.com",
+  databaseURL: "https://cweb2-dev-default-rtdb.firebaseio.com",
+  projectId: "cweb2-dev",
+  storageBucket: "cweb2-dev.appspot.com",
+  messagingSenderId: "236372839443",
+  appId: "1:236372839443:web:fbd34b280a692cba68c3e6",
+  measurementId: "G-TMHJQTH4G5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+const analytics = getAnalytics(app);
