f8005e3178ae2e9f4f36bbe1f395323c8f4366fc
subramaniyan <subramaniyansrinivasan2003@gmail.com>
2024-07-03 21:02:01 +0530

    made changes

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -9,8 +9,8 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.AUTH_URL,
-  projectId: process.env.PROJECT_ID,
+  databaseURL:process.env.AUTH_URL,
+  projectId: "cweb2-dev",
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MSGID,
   appId: process.env.APP_ID,
